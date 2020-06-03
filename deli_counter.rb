799a73060ecfc370a66e8afef3f5158bb5bbeda3
KHANDKER HASSAN <khassanasgar@gmail.com>
2020-06-03 04:52:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,11 +13,11 @@ take_a_number(katz_deli, "Sheni") #=> Welcome, Sheni. You are number 3 in line.
 
 
 def now_serving(array)
-  if array.empty? == true 
+  if array.empty? == true
     puts "There is nobody waiting to be served!"
-  elsif array.empty? == false 
+  elsif array.empty? == false
     puts "Currently serving #{array.shift}."
-  end 
-end 
+  end
+end
 
 now_serving(katz_deli)
