ae312a7ffd2f0b914be3cc79d18e1a4e9fa46c22
KHANDKER HASSAN <khassanasgar@gmail.com>
2020-06-03 04:47:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,34 +4,41 @@ take_a_number(katz_deli, "Anila") #=> Welcome, Anila. You are number 1 in line.
 take_a_number(katz_deli, "Sharz") #=> Welcome, Sharz. You are number 2 in line.
 take_a_number(katz_deli, "Sheni") #=> Welcome, Sheni. You are number 3 in line.
 
+katz_deli = [] 
+
 def take_a_number(array, name)
-  array.push (name)
-  position = array.index (name)
-  puts "Hello, #{name}. You are number #{array.index(name)+1} in line."
-  return name, position
-end
+  array.push(name)
+  position = array.index(name)
+  puts "Welcome, #{name}. You are number #{array.index(name)+1} in line."
+  return name, position 
+end 
 
-line (katz_deli) #=> "The line is currently: 1. Anila 2. Sharz 3. Sheni"
+take_a_number(katz_deli, "Anila")
+take_a_number(katz_deli, "Sharz")
+take_a_number(katz_deli, "Sheni")
+
+def line(array) 
+  if array.length == 0 
+    puts "The line is currently empty."
+  else 
+    message = "The line is currently:"
+    
+  array.each_with_index do |value, index| 
+    message += " #{index.to_i+1}. #{value}"
+  end 
+  puts "#{message}"
+  end 
+end 
+ 
+line(katz_deli)
 
 
-def line (array)
-  if array.length == 0
-  puts "The line is empty."
-else
-  message = "The line is currently:"
+def now_serving(array)
+  if array.empty? == true 
+    puts "There is nobody waiting to be served!"
+  elsif array.empty? == false 
+    puts "Currently serving #{array.shift}."
+  end 
+end 
 
-array.each_with_index do
-  |value, index|
-  message += "#{index.to_i+1} #{value}"
- end
- puts "#{message}"
- end
-end
-
-def now_serving (array)=
-if array.empty? == true
-   puts "Whos next!"
- elsif array.empty? == false
-   puts "Currently serving #{array.shift}."
- end
-end
+now_serving(katz_deli)
