38ec1583968aad58e504b67614025fee64f1bcab
KHANDKER HASSAN <khassanasgar@gmail.com>
2020-06-03 05:11:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,3 +1,4 @@
+
 katz_deli = []
 
 def take_a_number(array, name)
@@ -7,16 +8,18 @@ def take_a_number(array, name)
   return name, position
 end
 
-take_a_number(katz_deli, "Ada") #=> Welcome, Anila. You are number 1 in line.
-take_a_number(katz_deli, "Grace") #=> Welcome, Sharz. You are number 2 in line.
-take_a_number(katz_deli, "Kent") #=> Welcome, Sheni. You are number 3 in line.
+take_a_number(katz_deli, "Anila") #=> Welcome, Anila. You are number 1 in line.
+take_a_number(katz_deli, "Sheni") #=> Welcome, Sharz. You are number 2 in line.
+take_a_number(katz_deli, "Sharz") #=> Welcome, Sheni. You are number 3 in line.
 
 
-def now_serving(array)
-  if array.empty? == true
+
+def now_serving(katz_deli)
+  if katz_deli.empty?
     puts "There is nobody waiting to be served!"
-  elsif array.empty? == false
-    puts "Currently serving #{array.shift}."
+  else
+    puts "Currently serving #{katz_deli.first}."
+    katz_deli.shift
   end
 end
 
