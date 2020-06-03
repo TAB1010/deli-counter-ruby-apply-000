438700701579f3a24836cc0222001159306f8425
KHANDKER HASSAN <khassanasgar@gmail.com>
2020-06-03 04:30:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,39 @@
-# Write your code here.
+katz_deli = []
+
+def take_a_number(array, name)
+  array.push (name)
+  position = array.index (name)
+  puts "Hello, #{name}. You are number #{array.index(name)+1} in line."
+  return name, position
+end
+
+take_a_number(katz_deli, "Anila") #=> Welcome, Anila. You are number 1 in line.
+take_a_number(katz_deli, "Sharz") #=> Welcome, Sharz. You are number 2 in line.
+take_a_number(katz_deli, "Sheni") #=> Welcome, Sheni. You are number 3 in line.
+
+def line (array)
+  if array.length == 0
+  puts "The line is empty."
+else
+  message = "The line is currently:"
+
+array.each_with_index do
+  |value, index|
+  message += "#{index.to_i+1} #{value}"
+ end
+ puts "#{message}"
+ end
+end
+
+line (katz_deli) #=> "The line is currently: 1. Anila 2. Sharz 3. Sheni"
+
+
+
+def now_serving (array)
+  if array.empty == true
+    puts "You can go next!"
+  elsif array.empty == flase
+    puts "Now serving #{array.shift}."
+  end
+end
+now_serving(katz_deli) #=> "Currently serving Sharz."
